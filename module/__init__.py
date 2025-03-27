909378231b29a037b0c21784bff08798fd7168d7
sam perini <sam.perini@telenetgroup.be>
2025-03-27 14:09:26 +0100

    chore: update workspace to ignore

---
+++ module/__init__.py
@@ -0,0 +1,3 @@
+# This file marks the pages directory as a Python package.
+# It can be empty or contain initialization code for the package.
+# This file is necessary for Python to recognize the directory as a package.
